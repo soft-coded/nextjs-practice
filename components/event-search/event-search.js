d180f0a62f13fd837eb46d5e323f87fd52b356c5
shrutanten <shrutanten.work@gmail.com>
2024-03-23 22:28:47 +0530

    Add search functionality

--- components/event-search/event-search.js
+++ components/event-search/event-search.js
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useRouter } from "next/router";
 
 import styles from "./search.module.css";
 import Button from "../button/button";
 
 export default function EventSearch(props) {
+	const yearRef = useRef();
+	const monthRef = useRef();
+	const router = useRouter();
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		const year = yearRef.current.value;
+		const month = monthRef.current.value;
+
+		router.push(`/events/${year}/${month}`);
+	}
+
 	return (
-		<form className={styles.form}>
+		<form className={styles.form} onSubmit={handleSubmit}>
 			<div className={styles.controls}>
 				<div className={styles.control}>
 					<label htmlFor="year">Year</label>
-					<select name="year" id="year">
+					<select name="year" id="year" ref={yearRef}>
 						<option value="2021">2021</option>
 						<option value="2022">2022</option>
 					</select>
 				</div>
 				<div className={styles.control}>
 					<label htmlFor="month">Month</label>
-					<select name="month" id="month">
+					<select name="month" id="month" ref={monthRef}>
 						<option value="1">January</option>
 						<option value="2">February</option>
 						<option value="3">March</option>
