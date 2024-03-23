91ba9d424ad2f779f09940f0b5f4811780ec211b
shrutanten <shrutanten.work@gmail.com>
2024-03-23 22:49:39 +0530

    Add filter functionality

--- pages/events/[...slug]/index.js
+++ pages/events/[...slug]/index.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import { useRouter } from "next/router";
+import React from "react";
+
+import { getFilteredEvents } from "../../../dummy-data";
+import EventList from "../../../components/event-list/event-list";
 
 export default function EventsSlugPage() {
-  return (
-    <div>EventsSlugPage</div>
-  )
+	const slug = useRouter()?.query?.slug;
+	if (!slug) return null;
+
+	const [year, month] = slug;
+	const events = getFilteredEvents({ year: +year, month: +month });
+
+	return <EventList items={events} />;
 }
