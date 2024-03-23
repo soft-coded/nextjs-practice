8f43bfe057f9d99347b899e03be70d48d88386a1
shrutanten <shrutanten.work@gmail.com>
2024-03-23 20:02:41 +0530

    Attach styles

--- components/event-item/event-item.js
+++ components/event-item/event-item.js
@@ -7,17 +7,17 @@ export default function EventItem(props) {
 	return (
 		<li className={styles.item}>
 			<img src={"/" + props.image} alt={props.title} />
-			<div>
-				<div>
+			<div className={styles.content}>
+				<div className={styles.summary}>
 					<h2>{props.title}</h2>
-					<div>
+					<div className={styles.date}>
 						<time>{new Date(props.date).toLocaleDateString()}</time>
 					</div>
-					<div>
+					<div className={styles.address}>
 						<address>{props.location.replace(/, /g, "\n")}</address>
 					</div>
 				</div>
-				<div>
+				<div className={styles.actions}>
 					<Link href={`/events/${props.id}`}>Explore event</Link>
 				</div>
 			</div>
--- components/event-list/event-list.js
+++ components/event-list/event-list.js
@@ -1,9 +1,11 @@
 import React from "react";
+
 import EventItem from "../event-item/event-item";
+import styles from "./event-list.module.css";
 
 export default function EventList(props) {
 	return (
-		<ul>
+		<ul className={styles.list}>
 			{props.items.map((item, i) => (
 				<EventItem key={i} {...item} />
 			))}
